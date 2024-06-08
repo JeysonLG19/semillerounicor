35f0b63fc0bb758f5e0813e0f2ac87f92588ef45
JeysonLG19 <136490890+JeysonLG19@users.noreply.github.com>
2024-06-08 02:19:27 +0000

    componente principal

---
+++ components/principalSemilleros.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+    nombre: String,
+    programa: String,
+    facultad: String,
+})
+</script>
+<template>
+    <div>
+        <Card class="w-60">
+            <template #content>
+                <div class="flex flex-col justify-center items-center" >
+                    <Avatar image="https://logowik.com/content/uploads/images/universidad-de-cordoba8857.logowik.com.webp" shape="circle" size="xlarge" />
+                    <h2>{{ props.nombre }}</h2>
+                    <p>{{props.programa}}</p>
+                    <p>{{ props.facultad }}</p>
+                </div>
+            </template>
+        </Card>
+    </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
-<script></script>
+<script setup>
+import semilleros from '../data/semilleros.json'
+</script>
 <template>
-  <div></div>
+  <div class="w-100 p-3 flex flex-wrap gap-8 content-center">
+    <PrincipalSemilleros v-for="semillero in semilleros" :programa="semillero.programa" :facultad="semillero.facultad" :nombre="semillero.nombre"/>
+  </div>
 </template>
